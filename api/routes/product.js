71c9683f3b2f8ef102dbc4c430e2175af7cfa008
Aakriti <aakriti1711@gmail.com>
2018-05-31 15:55:32 -0700

    Mongosse validation & format response data.

--- api/routes/product.js
+++ api/routes/product.js
@@ -7,10 +7,26 @@ const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
-        .then(doc => {
-            console.log(doc);
-            res.status(200).json(doc);
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+
+                    }
+                })
+            }
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -30,8 +46,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: ' Handling post request to /products',
-                createdProduct: result
+                message: 'Created Prduct Successully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                }
             });
         })
         .catch(err => {
@@ -59,11 +83,17 @@ router.get('/:productId', (req, res, next) => {
     // }
 
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
-            console.log(doc);
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request:{
+                        type: 'GET',
+                        description: ' Get all Products',
+                        url: 'http://localhost:3000/products/'
+                }});
             } else {
                 res.status(404).json({
                     message: 'No valid entry found for ' + id
@@ -86,13 +116,19 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+            
+            res.status(200).json({
+                message:'Product updated',
+                request:{
+                    type:'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json({
-                error:err
+                error: err
             });
         });
 });
@@ -102,7 +138,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message : 'Product Deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products',
+                    body:{name: 'String', price :'Number' }
+                }
+            });
         })
         .catch(err => {
             console.log(err);
